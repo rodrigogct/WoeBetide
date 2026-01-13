4da9102ea23af62fa6b4d3aa57a23de1681fb1d3
Rodrigo Gomez del Campo <rodrigogomezdelcampo@hotmail.com>
2026-01-12 22:38:06 -0600

    Fix Cloudinary

--- WoeBetide/settings.py
+++ WoeBetide/settings.py
@@ -190,14 +190,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# if not DEBUG and os.getenv("CLOUDINARY_URL"):
-#     DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
-}
-
-if all(CLOUDINARY_STORAGE.values()):
+if not DEBUG and os.getenv("CLOUDINARY_URL"):
     DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+
