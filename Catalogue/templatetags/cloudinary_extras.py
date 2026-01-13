221f1299b31575aa64e0552c042007672ec10cff
Rodrigo Gomez del Campo <rodrigogomezdelcampo@hotmail.com>
2026-01-13 16:08:38 -0600

    Fix images webp display

---
+++ Catalogue/templatetags/cloudinary_extras.py
@@ -0,0 +1,15 @@
+from django import template
+register = template.Library()
+
+def _cld(url, transform):
+    if not url or "/upload/" not in url:
+        return url
+    return url.replace("/upload/", f"/upload/{transform}/", 1)
+
+@register.filter
+def cld_catalog(url):
+    return _cld(url, "f_webp,q_auto,a_auto,w_640,c_limit")
+
+@register.filter
+def cld_item(url):
+    return _cld(url, "f_webp,q_auto,a_auto,w_940,c_limit")
