6ec04258623206dd3cb60f0810e4708d847032c9
Rodrigo Gomez del Campo <rodrigogomezdelcampo@hotmail.com>
2026-01-30 09:47:40 -0600

    Cart img thumbnail fix

--- Catalogue/templatetags/catalog_cloudinary.py
+++ Catalogue/templatetags/catalog_cloudinary.py
@@ -16,4 +16,4 @@ def cld_item(url):
 
 @register.filter
 def cld_cart_thumb(url):
-    return _cld(url, "f_auto,q_auto,ca_auto,w_128,c_limit")
+    return _cld(url, "f_auto,q_auto,a_auto,w_128,c_limit")
