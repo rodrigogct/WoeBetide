245114f4ea05a0cc445bc4130d0cd14a76e59b10
Rodrigo Gomez del Campo <rodrigogomezdelcampo@hotmail.com>
2026-06-14 17:04:40 -0600

    Password fix

--- WoeBetide/settings.py
+++ WoeBetide/settings.py
@@ -82,11 +82,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
-    "Menu.middleware.SitePasswordMiddleware",
-
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+
+    'Menu.middleware.SitePasswordMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
